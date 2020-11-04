af2bb9ff375e1b8e667ab3e236c881c319d36cc7
Erick Canales <e94canales@gmail.com>
2020-11-03 17:52:31 -0800

    added meta to head

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export default function Home() {
     <Layout>
       <Head>
         <title>Erick Canales - Portfolio</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
