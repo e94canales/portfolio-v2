92980a18ff63b248053789f09e16c61e3cb1ea3a
Erick Canales <e94canales@gmail.com>
2020-11-05 19:26:25 -0800

    meta tags mobile support added

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,9 @@ export default function Home() {
     <Layout>
       <Head>
         <title>Erick Canales - Portfolio</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta name="HandheldFriendly" content="true"/>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
