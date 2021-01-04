116d50afc662744cd9dbf4548b2cee3173ab17b4
Erick Canales <e94canales@gmail.com>
2021-01-04 09:27:51 -0800

    added analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,22 @@
 import "../styles/globals.scss";
 import { Provider } from "react-redux";
 import store from "../redux";
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
+
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <Provider store={store}>
